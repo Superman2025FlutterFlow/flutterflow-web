0c533f3f023b390074d72b4c493a3efd2830a854
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-17 17:22:45 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/registro/registro_widget.dart
+++ lib/pages/registro/registro_widget.dart
@@ -367,7 +367,7 @@ class _RegistroWidgetState extends State<RegistroWidget> {
                 child: Padding(
                   padding: EdgeInsetsDirectional.fromSTEB(20.0, 14.0, 0.0, 5.0),
                   child: Text(
-                    'Confirma Contraseña',
+                    'Repite Contraseña',
                     style: FlutterFlowTheme.of(context).bodyMedium.override(
                           font: GoogleFonts.inter(
                             fontWeight: FontWeight.w500,
